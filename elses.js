1e4710347fd734c5d07cc7113ec512c1eeb0b8fe
CaioMDutra <caimcd.dutra@hotmail.com>
2022-08-24 17:21:20 -0300

    teste de commit

--- elses.js
+++ elses.js
@@ -4,7 +4,7 @@ let num2 = 21;
 let soma = num1+num2;
 
 if(soma > 20){
-    console.log(`numero muito grande`)
+    console.log(`numero muito grande para calcular`)
 }else{
     console.log(soma);
 }
